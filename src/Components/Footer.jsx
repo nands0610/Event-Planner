0566fc893fb2c3a3b70b67a88da3a70fedb7166b
Nanditha <sn06102005@gmail.com>
2025-06-21 12:20:36 +0530

    New changes: Added contents to sections, and created a footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <>
+            <footer>
+                <p>
+                    &copy; Event Planner Org.
+                </p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
